b65882ab84dd09b6c184e2023d5594cb32d35447
juan25mot <juanc.morantes@upb.edu.co>
2025-09-25 18:39:05 -0500

    product

---
+++ online_store.js
@@ -0,0 +1,16 @@
+// FAST & TASTY
+
+const productosComidaRapida = [
+  "Hamburguesa clásica",
+  "Hamburguesa con queso",
+  "Papas fritas",
+  "Nuggets de pollo",
+  "Pizza personal",
+  "Hot dog",
+  "Alitas de pollo",
+  "Aros de cebolla",
+  "Tacos",
+  "Burritos",
+  "Refresco grande",
+  "Malteada de vainilla",
+];
